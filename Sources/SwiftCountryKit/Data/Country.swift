877ce99257a7debb7b0ea7296037a729d9799278
yongbeomkwak <yongbeomkwak@gmail.com>
2025-01-07 17:05:14 +0900

    add public init on Countryt

--- Sources/SwiftCountryKit/Data/Country.swift
+++ Sources/SwiftCountryKit/Data/Country.swift
@@ -5,4 +5,10 @@ public struct Country: Decodable, Identifiable {
     public let code: String
     public let name: String
     public let number: String
+
+    public init(code: String, name: String, number: String) {
+        self.code = code
+        self.name = name
+        self.number = number
+    }
 }
