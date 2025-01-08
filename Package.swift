ed362d6e518ce06899eb234a6bf593017b7d7e0b
yongbeomkwak <yongbeomkwak@gmail.com>
2025-01-08 13:40:56 +0900

    rename value

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftCountryKit",
-            resources: [.process("Resources/countries.json")]),
+            name: "SwiftCountryKit"),
         .testTarget(
             name: "SwiftCountryKitTests",
             dependencies: ["SwiftCountryKit"]
--- Tests/SwiftCountryKitTests/SwiftCountryKitTests.swift
+++ Tests/SwiftCountryKitTests/SwiftCountryKitTests.swift
@@ -4,6 +4,14 @@ import XCTest
 final class SwiftCountryKitTests: XCTestCase {
     func testExample() throws {
         let countryKit = CountryProvider(initCountryCode: .us)
+        let allID = Locale.availableIdentifiers.sorted().count
+
+        print(allID)
+
+//        for id in allID {
+//            print(id)
+//        }
+
         XCTAssertEqual(countryKit.all.count, 240)
         XCTAssertNotEqual(countryKit.all.count, 0)
     }
